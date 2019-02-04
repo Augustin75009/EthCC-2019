a64650d51ca4fc8617b66c3eb522b9faf9bf674f
augustin cousin <augustincousin.pro@gmail.com>
2019-02-04 17:17:49 +0100

    links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 const speakersTemplate = document.getElementById("speakerTemplate")
-const speakersSection = document.getElementById("speakerSection")
 const speakersRow = document.getElementById("speakersRow")
 
 for (i = 0; i < 10; i++) {
-  console.log(speakerTemplate.childNodes)
   document.querySelector('#speakerTemplate .name').innerHTML = speakers[i].name;
   document.querySelector('#speakerTemplate img').setAttribute('src', speakers[i].picture);
   document.querySelector('#speakerTemplate .company').innerHTML = speakers[i].company;
