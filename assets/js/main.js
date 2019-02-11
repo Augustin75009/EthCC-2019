a42eac45217b634811b3ec9c579bbf4133ee1760
augustin cousin <augustincousin.pro@gmail.com>
2019-02-11 12:11:46 +0100

    added tooltip ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,13 @@ const speakersRow = document.getElementById("speakersRow")
 
 
 for (i = 0; i < 10; i++) {
-  document.querySelector('#speakerTemplate .name').innerHTML = speakers[i].name;
+
+  if(speakers[i].link != undefined && speakers[i].link != '') {
+    document.querySelector('#speakerTemplate .name').innerHTML = (`<a href="`+speakers[i].link+`">`+speakers[i].name+`</a>`);
+  } else {
+    document.querySelector('#speakerTemplate .name').innerHTML = speakers[i].name;
+  }
+
   document.querySelector('#speakerTemplate img').setAttribute('src', speakers[i].picture);
   document.querySelector('#speakerTemplate .company').innerHTML = speakers[i].company;
   speakersRow.appendChild(speakersTemplate.childNodes[1].cloneNode(true));
